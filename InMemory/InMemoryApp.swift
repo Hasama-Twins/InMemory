9cf5b019bd92e4366d260a4181723b835edaa207
Summer Hasama <summerhasama@gmail.com>
2024-04-13 15:01:51 -0700

    Initial Commit

---
+++ InMemory/InMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InMemoryApp.swift
+//  InMemory
+//
+//  Created by Summer Hasama on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct InMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
